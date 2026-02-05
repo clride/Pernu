bc0817348901eaa3bb8432349df3a7cd78307f5e
clride <david.g.schilcher@gmail.com>
2026-02-05 19:43:19 +0100

    Remove get_msg as we will be using sockets
    Sockets will work nicer here instead of http requests

--- backend/webserver.py
+++ backend/webserver.py
@@ -11,9 +11,6 @@ FRONTEND_DIR = os.path.join(PROJECT_ROOT, "frontend")
 
 flaskapp = Flask(__name__)
 
-global messages
-messages: list[str] = []
-
 @flaskapp.route('/')
 def home():
     with open(os.path.join(FRONTEND_DIR, "index.html")) as f:
@@ -59,27 +56,6 @@ def login():
         return jsonify({"status": "Success!"}), 200
     else:
         return jsonify({"status": "Invalid Username or Password!"}), 401
-
-@flaskapp.route('/msg', methods=['POST'])
-def msg():
-    data = request.get_json()
-    auth = data.get('auth')
-    if not db.is_valid_user(auth.get('username'), auth.get('password')):
-        return jsonify({"error": "Authentication failed"}), 401
-    content = data.get('content')
-    messages.append(content)
-    return jsonify({"status": "Message received"}), 200
-
-@flaskapp.route('/msg', methods=['GET'])
-def get_msg():
-    data = request.get_json()
-    auth = data.get('auth')
-    if not db.is_valid_user(auth.get('username'), auth.get('password')):
-        return jsonify({"error": "Authentication failed"}), 401
-    
-    msgs = jsonify(messages)
-    return jsonify({"messages": msgs}), 200
-
 
 if __name__ == '__main__':
     print(sys.path)
